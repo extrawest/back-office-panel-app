da6c2e93a55647736bca560db0e7dc1a6daa60ac
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-15 17:34:41 +0200

    Added modules

--- apps/office-mui/src/app/app.module.ts
+++ apps/office-mui/src/app/app.module.ts
@@ -2,11 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './views/login/login.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconRegistry } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule,
+    HttpClientModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule
+  ],
+  providers: [MatIconRegistry],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
