fa03b21aacebb6a0b0fc9f707b6ddfd736bf1552
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-15 20:20:04 +0200

    Developed register page

---
+++ apps/office-mui/src/app/views/register/components/register/register.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'office-app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  form: FormGroup;
+  toggleShowPassword = true;
+  constructor(
+    private fb: FormBuilder,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'facebook-icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        './../../../../../assets/icons/facebook-icon.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'google-icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        './../../../../../assets/icons/google-icon.svg'
+      )
+    );
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  isValidForm(): boolean {
+    return this.form.invalid || this.form.pristine;
+  }
+}
