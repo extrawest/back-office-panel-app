dd0cecd6aafac7da30eed7c543bc5cd6ae642ef4
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-16 15:23:15 +0200

    Made login module and its component

---
+++ apps/office-prime-ng/src/app/views/login/login.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+];
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+  ],
+})
+export class LoginModule {}
