2d9883231c69ed299e812275a1b2cfd00c986da1
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-19 17:51:17 +0200

    Added register component

---
+++ apps/office-tailwind/src/app/views/register/components/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'office-app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  form: FormGroup;
+  toggleShowPassword = true;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  isValidForm(): boolean {
+    return this.form.invalid || this.form.pristine;
+  }
+}
