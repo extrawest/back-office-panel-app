5fd4c6322e018d44af5dcc5b1bc250be5dc6c45f
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-27 16:04:30 +0200

    Added routes for navigation between side-menu items

--- apps/office-mui/src/app/views/side-menu/side-menu.module.ts
+++ apps/office-mui/src/app/views/side-menu/side-menu.module.ts
@@ -1,30 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: SideMenuComponent,
-  },
-];
+import { SideMenuRoutes } from './side-menu.routes';
 
 @NgModule({
   declarations: [SideMenuComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     MatSidenavModule,
     MatListModule,
+    SideMenuRoutes,
   ],
 })
 export class SideMenuModule {}
---
+++ apps/office-mui/src/app/views/side-menu/side-menu.routes.ts
@@ -0,0 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: SideMenuComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./../../views/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+    ],
+  },
+];
+
+export const SideMenuRoutes = RouterModule.forChild(routes);
