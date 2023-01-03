6f0442b245f05d2d3e3fa039f12fbf3d13eeb047
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-03 20:49:09 +0200

    Implemented logic to display modal window

--- apps/office-mui/src/app/views/client/components/client/client.component.ts
+++ apps/office-mui/src/app/views/client/components/client/client.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { AddTicketComponent } from './../add-ticket/add-ticket.component';
+import { PriorityEnum } from '@office-app/services/priority-enum';
 
 @Component({
   selector: 'office-app-client',
@@ -13,7 +20,13 @@ import { MatTableDataSource } from '@angular/material/table';
 export class ClientComponent {
   displayedColumns: string[] = ['details', 'name', 'date', 'priority'];
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  ticketDetails: string;
+  customerName: string;
+  date: string;
+  priority: PriorityEnum;
+
   constructor(
+    public dialog: MatDialog,
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer
   ) {
@@ -29,5 +42,20 @@ export class ClientComponent {
         './assets/icons/filter-icon.svg'
       )
     );
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddTicketComponent, {
+      data: {
+        ticketDetails: this.ticketDetails,
+        customerName: this.customerName,
+        date: this.date,
+        priority: this.priority,
+      },
+      width: '55%',
+      height: '50%',
+    });
+
+    dialogRef.afterClosed().subscribe();
   }
 }
