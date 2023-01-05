1d1ea0c857f208dfb5952d5cbc71c5effff6a9d3
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-05 17:42:16 +0200

    Added new modules

---
+++ apps/office-prime-ng/src/app/views/dashboard/dashboard.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardMainComponent } from './components/dashboard-main/dashboard-main.component';
+import { DashboardChartComponent } from './components/dashboard-chart/dashboard-chart.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { DashboardTaskComponent } from './components/dashboard-task/dashboard-task.component';
+import { DashboardTicketComponent } from './components/dashboard-ticket/dashboard-ticket.component';
+import { DividerModule } from 'primeng/divider';
+import { CheckboxModule } from 'primeng/checkbox';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardMainComponent,
+  },
+];
+
+@NgModule({
+  declarations: [
+    DashboardMainComponent,
+    DashboardChartComponent,
+    DashboardTaskComponent,
+    DashboardTicketComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DividerModule,
+    CheckboxModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
+})
+export class DashboardModule {}
