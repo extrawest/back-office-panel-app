a64370619c02096b789163cd1fdcbfb132003e56
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-05 14:57:58 +0200

    Added new const at lib

---
+++ libs/services/src/lib/consts/menu-item.const.ts
@@ -0,0 +1,28 @@
+export const menuItems = [
+  {
+    name: 'Dashboard',
+    url: '/home/dashboard',
+    image: './assets/icons/info-icon.png',
+  },
+  {
+    name: 'Team',
+    image: './assets/icons/team-icon.png',
+  },
+  {
+    name: 'Offers',
+    image: './assets/icons/offer-icon.png',
+  },
+  {
+    name: 'Partners',
+    image: './assets/icons/call-center-icon.png',
+  },
+  {
+    name: 'Finances',
+    image: './assets/icons/finance-icon.png',
+  },
+  {
+    name: 'Clients',
+    url: '/home/clients',
+    image: './assets/icons/man-icon.png',
+  },
+];
