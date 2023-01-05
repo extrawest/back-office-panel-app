23e3aee5db1424b338d36f4b443686aef56c0bf6
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-05 12:34:06 +0200

    Implemented logic for register page

--- apps/office-prime-ng/src/app/views/register/components/register/register.component.ts
+++ apps/office-prime-ng/src/app/views/register/components/register/register.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@office-app/services/auth-service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'office-app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy {
   form: FormGroup;
   toggleShowPassword = true;
-  constructor(private fb: FormBuilder) {
+  registerSubscription: Subscription | undefined;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.form = this.fb.group({
       name: ['', [Validators.required]],
       password: ['', [Validators.required]],
@@ -17,7 +25,22 @@ export class RegisterComponent {
     });
   }
 
-  isValidForm(): boolean {
+  public isValidForm(): boolean {
     return this.form.invalid || this.form.pristine;
+  }
+
+  public onSubmit() {
+    const { name, email, password } = this.form.getRawValue();
+    this.registerSubscription = this.authService.register(
+      email,
+      password,
+      name
+    );
+  }
+
+  public ngOnDestroy(): void {
+    if (this.registerSubscription) {
+      this.registerSubscription.unsubscribe();
+    }
   }
 }
