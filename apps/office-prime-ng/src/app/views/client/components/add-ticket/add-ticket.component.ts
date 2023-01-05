e3f1ad5c7d33e7c7a5c71e59f9958f907cfb5147
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-05 19:00:24 +0200

    Generated add-ticket component

---
+++ apps/office-prime-ng/src/app/views/client/components/add-ticket/add-ticket.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'office-app-add-ticket',
+  templateUrl: './add-ticket.component.html',
+  styleUrls: ['./add-ticket.component.scss'],
+})
+export class AddTicketComponent {}
