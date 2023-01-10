ebcf591587734efdf396f81af5aefed21f16b50e
Bohdan Horodyskyi <bohdan.horodyskyi@extrawest.com>
2023-01-10 13:46:46 +0200

    Implemented dashboard-ticket component for ngzorro project

---
+++ apps/office-ant/src/app/views/dashboard/components/dashboard-ticket/dashboard-ticket.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'office-app-dashboard-ticket',
+  templateUrl: './dashboard-ticket.component.html',
+  styleUrls: ['./dashboard-ticket.component.less'],
+})
+export class DashboardTicketComponent {}
