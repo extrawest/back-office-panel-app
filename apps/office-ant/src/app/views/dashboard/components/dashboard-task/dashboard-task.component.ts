df51d1f6a200010a29cafedb0eddba096144e851
Bohdan Horodyskyi <bohdan.horodyskyi@extrawest.com>
2023-01-10 13:46:23 +0200

    Implemented dashboard-task component for ngzorro project

---
+++ apps/office-ant/src/app/views/dashboard/components/dashboard-task/dashboard-task.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'office-app-dashboard-task',
+  templateUrl: './dashboard-task.component.html',
+  styleUrls: ['./dashboard-task.component.less'],
+})
+export class DashboardTaskComponent {}
