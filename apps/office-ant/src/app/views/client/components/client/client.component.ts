3cbc1ecba37e0e5c6b2a2a984e561ab70e48f6a5
Bohdan Horodyskyi <bohdan.horodyskyi@extrawest.com>
2023-01-10 17:23:34 +0200

    Implemented logic and UI of client page

---
+++ apps/office-ant/src/app/views/client/components/client/client.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Ticket } from '@office-app/services/ticket-interface';
+import { UserService } from '@office-app/services/user-service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'office-app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.less'],
+})
+export class ClientComponent implements OnDestroy {
+  public ticketsArray: Ticket[] = [];
+  public displayModal: boolean;
+  public headers: string[] = [
+    'Ticket details',
+    'Customer name',
+    'Date',
+    'Priority',
+  ];
+  private componentDestroyed$: Subject<void> = new Subject();
+  constructor(private userService: UserService) {
+    this.getTickets();
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
+  }
+
+  public showModalDialog() {
+    this.displayModal = true;
+  }
+
+  public closeAccountModal(event: boolean) {
+    if (event) {
+      this.displayModal = false;
+      this.getTickets();
+    }
+  }
+
+  private getTickets() {
+    this.userService
+      .getUserTickets()
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe((result) => {
+        if (result) {
+          Object.values(result).forEach((value: any) => {
+            this.ticketsArray.push(value);
+          });
+          this.ticketsArray.reverse();
+        }
+      });
+    this.ticketsArray = [];
+  }
+}
