f7697c7b9a7929a44887ea54829d93fa01d81ce7
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-13 11:31:47 +0200

    Removed unnecessary styles from client page

--- apps/office-mui/src/app/views/client/client.module.ts
+++ apps/office-mui/src/app/views/client/client.module.ts
@@ -13,6 +13,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 
 const routes: Routes = [
   {
@@ -36,6 +37,7 @@ const routes: Routes = [
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatCardModule
   ],
   providers: [MatIconRegistry],
 })
