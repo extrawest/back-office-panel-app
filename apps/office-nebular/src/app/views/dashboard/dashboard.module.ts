0ae5ff248dfe63393dc2f664fa0bdbebac351387
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-15 12:09:45 +0200

    Developed Dashboard module

--- apps/office-nebular/src/app/views/dashboard/dashboard.module.ts
+++ apps/office-nebular/src/app/views/dashboard/dashboard.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardMainComponent } from './components/dashboard-main/dashboard-main.component';
+import { DashboardChartComponent } from './components/dashboard-chart/dashboard-chart.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { DashboardTaskComponent } from './components/dashboard-task/dashboard-task.component';
+import { DashboardTicketComponent } from './components/dashboard-ticket/dashboard-ticket.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardMainComponent,
+  },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardMainComponent,
+    DashboardChartComponent,
+    DashboardTaskComponent,
+    DashboardTicketComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
