e833e0f708d93a9fb2b7719d478bc3c8e5645e27
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-25 10:39:33 +0200

    Added modules and components

--- apps/office-prime-ng/src/app/views/dashboard/dashboard.module.ts
+++ apps/office-prime-ng/src/app/views/dashboard/dashboard.module.ts
@@ -10,6 +10,12 @@ import { DashboardTaskComponent } from './components/dashboard-task/dashboard-ta
 import { DashboardTicketComponent } from './components/dashboard-ticket/dashboard-ticket.component';
 import { DividerModule } from 'primeng/divider';
 import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { DashboardTaskDialogComponent } from './components/dashboard-task-dialog/dashboard-task-dialog.component';
+import { DashboardTicketDialogComponent } from './components/dashboard-ticket-dialog/dashboard-ticket-dialog.component';
 
 const routes: Routes = [
   {
@@ -24,6 +30,8 @@ const routes: Routes = [
     DashboardChartComponent,
     DashboardTaskComponent,
     DashboardTicketComponent,
+    DashboardTaskDialogComponent,
+    DashboardTicketDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -33,6 +41,10 @@ const routes: Routes = [
     HttpClientModule,
     DividerModule,
     CheckboxModule,
+    ButtonModule,
+    DialogModule,
+    DropdownModule,
+    InputTextModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
     }),
