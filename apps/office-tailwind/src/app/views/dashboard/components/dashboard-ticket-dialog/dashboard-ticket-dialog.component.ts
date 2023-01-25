21fd4b68b5a6a18ee5eea3203246f3146e1f7087
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-25 14:35:10 +0200

    Implemented dialog window to create new ticket

---
+++ apps/office-tailwind/src/app/views/dashboard/components/dashboard-ticket-dialog/dashboard-ticket-dialog.component.ts
@@ -0,0 +1,45 @@
+import { Component, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '@office-app/services/user-service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'office-app-dashboard-ticket-dialog',
+  templateUrl: './dashboard-ticket-dialog.component.html',
+  styleUrls: ['./dashboard-ticket-dialog.component.css'],
+})
+export class DashboardTicketDialogComponent implements OnDestroy {
+  @Output() isModalClosed = new EventEmitter<boolean>();
+  public form: FormGroup;
+  private componentDestroyed$: Subject<void> = new Subject();
+  public isModalVisible = true;
+  constructor(private userService: UserService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      ticketName: [''],
+      ticketNumber: [''],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
+  }
+
+  public closeModal() {
+    this.isModalVisible = false;
+    this.isModalClosed.emit(true);
+  }
+
+  public addUnresolvedTicket() {
+    const { ticketName, ticketNumber } = this.form.getRawValue();
+    this.userService
+      .addUnresolvedTicket(ticketName, ticketNumber)
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe({
+        complete: () => {
+          this.closeModal();
+        },
+      });
+  }
+}
