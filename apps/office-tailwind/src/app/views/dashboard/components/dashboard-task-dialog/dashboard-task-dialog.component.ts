d18bc6669f30ee1fac78814006a649c22f7ecdfa
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-25 14:34:53 +0200

    Implemented dialog window to create new task

---
+++ apps/office-tailwind/src/app/views/dashboard/components/dashboard-task-dialog/dashboard-task-dialog.component.ts
@@ -0,0 +1,47 @@
+import { Component, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '@office-app/services/user-service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { TaskTypes } from '@office-app/services/task-types';
+
+@Component({
+  selector: 'office-app-dashboard-task-dialog',
+  templateUrl: './dashboard-task-dialog.component.html',
+  styleUrls: ['./dashboard-task-dialog.component.css'],
+})
+export class DashboardTaskDialogComponent implements OnDestroy {
+  @Output() isModalClosed = new EventEmitter<boolean>();
+  public form: FormGroup;
+  public isModalVisible = true;
+  public statuses = TaskTypes;
+  private componentDestroyed$: Subject<void> = new Subject();
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.form = this.fb.group({
+      taskName: [''],
+      taskStatus: [''],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
+  }
+
+  public closeModal() {
+    this.isModalVisible = false;
+    this.isModalClosed.emit(true);
+  }
+
+  public addTask() {
+    const { taskName, taskStatus } = this.form.getRawValue();
+    this.userService
+      .addTask(taskName, taskStatus)
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe({
+        complete: () => {
+          this.closeModal();
+        },
+      });
+  }
+}
