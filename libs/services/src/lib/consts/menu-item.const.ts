08c80dff4efd83e40e562f495e39ceb799ac063c
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-01-31 17:43:42 +0200

    Added new key to menuItems for styling elements of side menu

--- libs/services/src/lib/consts/menu-item.const.ts
+++ libs/services/src/lib/consts/menu-item.const.ts
@@ -3,26 +3,32 @@ export const menuItems = [
     name: 'Dashboard',
     url: '/home/dashboard',
     image: './assets/icons/info-icon.png',
+    styles: "cursor: pointer"
   },
   {
     name: 'Team',
     image: './assets/icons/team-icon.png',
+    styles: "cursor: not-allowed"
   },
   {
     name: 'Offers',
     image: './assets/icons/offer-icon.png',
+    styles: "cursor: not-allowed"
   },
   {
     name: 'Partners',
     image: './assets/icons/call-center-icon.png',
+    styles: "cursor: not-allowed"
   },
   {
     name: 'Finances',
     image: './assets/icons/finance-icon.png',
+    styles: "cursor: not-allowed"
   },
   {
     name: 'Clients',
     url: '/home/clients',
     image: './assets/icons/man-icon.png',
+    styles: "cursor: pointer"
   },
 ];
