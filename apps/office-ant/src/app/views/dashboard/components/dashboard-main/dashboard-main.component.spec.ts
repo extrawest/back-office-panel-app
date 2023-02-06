1d52b2495132503a4c389809a061c333fee1eb49
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-06 14:24:00 +0200

    Implemented tests for dashboard main component

--- apps/office-ant/src/app/views/dashboard/components/dashboard-main/dashboard-main.component.spec.ts
+++ apps/office-ant/src/app/views/dashboard/components/dashboard-main/dashboard-main.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardMainComponent } from './dashboard-main.component';
+import { DashboardChartComponent } from './../dashboard-chart/dashboard-chart.component';
 
 describe('DashboardMainComponent', () => {
   let component: DashboardMainComponent;
@@ -8,7 +9,7 @@ describe('DashboardMainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DashboardMainComponent],
+      declarations: [DashboardMainComponent, DashboardChartComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardMainComponent);
@@ -18,5 +19,29 @@ describe('DashboardMainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    expect(fixture.nativeElement.querySelector('h1')?.textContent).toContain(
+      'Dashboard'
+    );
+  });
+
+  it('should render chart component', () => {
+    expect(
+      fixture.nativeElement.querySelector('office-app-dashboard-chart')
+    ).toBeTruthy();
+  });
+
+  it('should render ticket component', () => {
+    expect(
+      fixture.nativeElement.querySelector('office-app-dashboard-ticket')
+    ).toBeTruthy();
+  });
+
+  it('should render task component', () => {
+    expect(
+      fixture.nativeElement.querySelector('office-app-dashboard-task')
+    ).toBeTruthy();
   });
 });
