280ab4d2f2ad40a7def8e8631932a7f50570e2cd
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-07 20:20:23 +0200

    Refactored test file of SideMenuComponent

--- apps/office-ant/src/app/views/side-menu/components/side-menu/side-menu.component.spec.ts
+++ apps/office-ant/src/app/views/side-menu/components/side-menu/side-menu.component.spec.ts
@@ -6,57 +6,47 @@ import {
 } from '@angular/core/testing';
 import { SideMenuComponent } from './side-menu.component';
 import { LoginComponent } from './../../../login/components/login/login.component';
-import { UserService } from '@office-app/services/user-service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SideMenuModule } from './../../side-menu.module';
 import { AuthService } from '@office-app/services/auth-service';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from './../../../../../environments/environment.prod';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 describe('SideMenuComponent', () => {
   let component: SideMenuComponent;
   let fixture: ComponentFixture<SideMenuComponent>;
-  let spy: jasmine.Spy;
-  let userService: UserService;
-  let router: Router;
-  let location: Location;
 
   beforeEach(async () => {
-    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
     await TestBed.configureTestingModule({
       imports: [
-        ReactiveFormsModule,
+        SideMenuModule,
         RouterTestingModule.withRoutes([
           { path: 'login', component: LoginComponent },
         ]),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
       ],
       declarations: [SideMenuComponent],
-      providers: [AuthService, { provide: Router, useValue: routerSpy }],
+      providers: [AuthService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SideMenuComponent);
     component = fixture.componentInstance;
-    spy = spyOn(userService, 'addProfileImage').and.callThrough();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should have a name of user', () => {
-    fixture.whenStable().then(() => {
-      expect(component.userName.length).not.toBe(0);
-    });
-  });
-  it('should render 6 menu items', () => {
-    expect(component.menuItems.length).toBe(6);
-  });
-  it('should redirect to login page after logout', fakeAsync(() => {
-    router = TestBed.inject(Router);
-    location = TestBed.inject(Location);
-    router.initialNavigation();
-    component.onLogout();
+
+  it('should have a name of user', fakeAsync(() => {
     tick();
-    expect(location.path()).toBe('/login');
+    expect(component.userName?.length).not.toBe(0);
+  }));
+
+  it('should render 6 menu items', fakeAsync(() => {
+    tick();
+    expect(component.menuItems.length).toEqual(6);
   }));
 });
