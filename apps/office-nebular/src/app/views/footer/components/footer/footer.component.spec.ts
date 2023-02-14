e06d0caeaf0c1f44b3f2e34526ef6b8e8169bc13
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-14 19:36:00 +0200

    Added tests for FooterComponent

--- apps/office-nebular/src/app/views/footer/components/footer/footer.component.spec.ts
+++ apps/office-nebular/src/app/views/footer/components/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { FooterModule } from './../../footer.module';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -9,6 +10,7 @@ describe('FooterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FooterComponent],
+      imports: [FooterModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
@@ -18,5 +20,10 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display image logo', () => {
+    const logo = fixture.debugElement.nativeElement.querySelector('img');
+    expect(logo['src']).toContain('EW-logo-horizontal.png');
   });
 });
