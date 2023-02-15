9853a400d1c789631c63fc640b9e8838c212086b
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-15 14:27:22 +0200

    Removed some default tests

--- apps/office-prime-ng/src/app/app.component.spec.ts
+++ apps/office-prime-ng/src/app/app.component.spec.ts
@@ -16,18 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'office-prime-ng'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('office-prime-ng');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome office-prime-ng'
-    );
-  });
 });
