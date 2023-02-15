9bf4c2d17def0f216faa847acf3efadbbdd31b7d
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-15 14:30:23 +0200

    Implemented tests for DashboardTaskDialogComponent

--- apps/office-prime-ng/src/app/views/dashboard/components/dashboard-task-dialog/dashboard-task-dialog.component.spec.ts
+++ apps/office-prime-ng/src/app/views/dashboard/components/dashboard-task-dialog/dashboard-task-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardTaskDialogComponent } from './dashboard-task-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardModule } from './../../dashboard.module';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from './../../../../../environments/environment';
 
 describe('DashboardTaskDialogComponent', () => {
   let component: DashboardTaskDialogComponent;
@@ -8,6 +12,12 @@ describe('DashboardTaskDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        DashboardModule,
+        BrowserAnimationsModule,
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+      ],
       declarations: [DashboardTaskDialogComponent],
     }).compileComponents();
 
@@ -18,5 +28,43 @@ describe('DashboardTaskDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    component.form.controls['taskName'].setValue('');
+    component.form.controls['taskStatus'].setValue('');
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('taskName field validity', () => {
+    const taskName = component.form.controls['taskName'];
+    expect(taskName.valid).toBeFalsy();
+    taskName.setValue('');
+    expect(taskName.hasError('required')).toBeTruthy();
+  });
+
+  it('taskStatus field validity', () => {
+    const taskStatus = component.form.controls['taskStatus'];
+    expect(taskStatus.valid).toBeFalsy();
+    taskStatus.setValue('');
+    expect(taskStatus.hasError('required')).toBeTruthy();
+  });
+
+  it('form should be valid', () => {
+    component.form.controls['taskName'].setValue('test');
+    component.form.controls['taskStatus'].setValue('NEW');
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('should have 3 task statuses value', () => {
+    fixture.whenStable().then(() => {
+      expect(component.statuses.length).toEqual(3);
+    });
+  });
+
+  it('should close modal', () => {
+    component.closeModal();
+    fixture.detectChanges();
+    expect(component.isModalVisible).toBe(false);
   });
 });
