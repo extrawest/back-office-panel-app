272c97065e401ce200a37bfedae03d5921490b0b
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-15 17:33:50 +0200

    Added test to AppComponent to check title

--- apps/office-prime-ng/src/app/app.component.spec.ts
+++ apps/office-prime-ng/src/app/app.component.spec.ts
@@ -16,4 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it(`should have as title 'office-prime-ng'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('office-prime-ng');
+  });
+
 });
