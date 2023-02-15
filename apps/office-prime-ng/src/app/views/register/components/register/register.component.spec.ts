f5d729b30196f7546e41dda795ad6c18cf3fb812
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-15 14:34:04 +0200

    Implemented tests for RegisterComponent

--- apps/office-prime-ng/src/app/views/register/components/register/register.component.spec.ts
+++ apps/office-prime-ng/src/app/views/register/components/register/register.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import { RegisterModule } from './../../register.module';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from './../../../../../environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +12,14 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RegisterModule,
+        RouterTestingModule,
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+      ],
+      declarations: [RegisterComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
@@ -19,5 +28,40 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    component.form.controls['name'].setValue('');
+    component.form.controls['password'].setValue('');
+    component.form.controls['email'].setValue('');
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('username field validity', () => {
+    const name = component.form.controls['name'];
+    expect(name.valid).toBeFalsy();
+    name.setValue('');
+    expect(name.hasError('required')).toBeTruthy();
+  });
+
+  it('password field validity', () => {
+    const password = component.form.controls['password'];
+    expect(password.valid).toBeFalsy();
+    password.setValue('');
+    expect(password.hasError('required')).toBeTruthy();
+  });
+
+  it('email field validity', () => {
+    const email = component.form.controls['email'];
+    expect(email.valid).toBeFalsy();
+    email.setValue('');
+    expect(email.hasError('required')).toBeTruthy();
+  });
+
+  it('form should be valid', () => {
+    component.form.controls['email'].setValue('test@test.com');
+    component.form.controls['password'].setValue('test');
+    component.form.controls['name'].setValue('testUser');
+    expect(component.form.valid).toBeTruthy();
   });
 });
