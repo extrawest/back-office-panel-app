54fc0cd9d5bb43c2607c124607f616b44a634bcc
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-15 19:21:43 +0200

    Added e2e tests to office-nebular project

--- apps/office-nebular-e2e/src/e2e/app.cy.ts
+++ apps/office-nebular-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,28 @@
-import { getGreeting } from '../support/app.po';
-
 describe('office-nebular', () => {
-  beforeEach(() => cy.visit('/'));
+  it('should display place to reset password', () => {
+    cy.visit('/login');
+    cy.url().should('includes', '/login');
+    cy.get('a').click();
+    cy.contains('button', 'Submit').should('be.visible');
+  });
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should link to register page', () => {
+    cy.visit('/login');
+    cy.contains('p', 'Register here').click();
+    cy.url().should('includes', '/register');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome office-nebular');
+  it('should register new user', () => {
+    const randomString = () =>
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+    const email = `${randomString()}@${randomString()}.test`;
+    cy.visit('/register');
+    cy.url().should('includes', 'register');
+    cy.get('input[type="text"]').type('userName');
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type('test123');
+    cy.contains('button', 'Register').click();
+    cy.contains('a', 'Log out').should('be.visible');
   });
 });
