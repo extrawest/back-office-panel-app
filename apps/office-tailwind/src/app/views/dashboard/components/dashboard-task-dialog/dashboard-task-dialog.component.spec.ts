6bf5942859fd1acc128035d6a0106d1bd979e2a5
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2023-02-17 11:25:35 +0200

    Fixed tests that had no expectations at tailwind project

--- apps/office-tailwind/src/app/views/dashboard/components/dashboard-task-dialog/dashboard-task-dialog.component.spec.ts
+++ apps/office-tailwind/src/app/views/dashboard/components/dashboard-task-dialog/dashboard-task-dialog.component.spec.ts
@@ -56,10 +56,8 @@ describe('DashboardTaskDialogComponent', () => {
     expect(component.form.valid).toBeTruthy();
   });
 
-  it('should have 3 task statuses value', () => {
-    fixture.whenStable().then(() => {
-      expect(component.statuses.length).toEqual(3);
-    });
+  it('should have 3 task statuses value', function () {
+    expect(component.statuses.length).toBe(3);
   });
 
   it('should close modal', () => {
